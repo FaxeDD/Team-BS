f9b3fa2809d3f0f73d80cfa3ee0cd8db285ca6a4
Drkhaan <maxime.lorin@e-artsup.com>
2021-01-19 18:09:03 +0100

    detection enemy raycast

---
+++ Assets/Bilou/Script/Shooting.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    
+RaycastHit hit;
+public GameObject Bullet;
+
+public Transform Canon;
+void Update()
+{        
+    if (Input.GetMouseButtonDown(1))
+    {
+        if ( Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition) , out hit, Mathf.Infinity))
+        {
+            if ( hit.transform.gameObject.tag == "Enemy" )
+            {
+                Shoot();
+                Debug.Log("tst");
+            }
+        }
+    }
+}
+
+void Shoot()
+{
+    //GameObject bulletObj = Instantiate(Bullet , Canon.position, Canon.rotation);
+}
+
+}
